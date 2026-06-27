71d2c07cbfe8c05c875bdefb40530a05df9cf717
Quirin Pflaum <0goath@gmail.com>
2026-06-27 18:53:12 +0200

    Fix absolute paths for reliable deployment

--- scripts/auth_guard.js
+++ scripts/auth_guard.js
@@ -13,6 +13,6 @@
  */
 (function guardRoute() {
     if (!localStorage.getItem('currentUser')) {
-        window.location.replace('/index.html');
+        window.location.replace('../index.html');
     }
 })();
